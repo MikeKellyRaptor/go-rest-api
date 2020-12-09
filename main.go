928fb7a06ae2f7c0adb2c33a6e5b36c27ade4953
Mike Kelly <mike.kelly@raptor-con.com>
2020-12-09 11:36:30 +0000

    Add basic home endpoint routing.

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home buddy!")
+}
 
 func main() {
-	fmt.Println("Hi there Mike!")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
+	//fmt.Println("Hi there Mike!")
 }
